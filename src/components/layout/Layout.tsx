4f25f91d71b5ea18ecacf16498f4fd9b4f014c4f
Grind <bellenlowell@gmail.com>
2023-09-05 18:22:38 +0800

    commit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,14 +3,18 @@ import Header from '../header/Header'
 import { Outlet } from 'react-router-dom'
 
 type LayoutProp = {
-    theme: string,
-    toggleTheme: () => void
+  theme: string,
+  toggleTheme: () => void
 }
-const Layout = ({theme, toggleTheme}: LayoutProp) => {
+const Layout = ({ theme, toggleTheme }: LayoutProp) => {
   return (
     <div className="layout-container">
+      <div className="layout-left">
         <Header theme={theme} toggleTheme={toggleTheme} />
+      </div>
+      <div className="layout-right">
         <Outlet />
+      </div>
     </div>
   )
 }
