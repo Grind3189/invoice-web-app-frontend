1f203fe37633443adcfd5cc7c83c6e8695441acd
Grind <bellenlowell@gmail.com>
2023-09-11 20:45:46 +0800

    fixed backdrop bug

--- src/components/backdrop/Backdrop.tsx
+++ src/components/backdrop/Backdrop.tsx
@@ -1,12 +1,20 @@
-import './backdrop.scss'
+import "./backdrop.scss"
 type BackdropProp = {
-    children: React.ReactNode
-    toggleShow: () => void,
-    show: boolean
+  children: React.ReactNode
+  toggleShow: () => void
+  show: boolean
 }
-const Backdrop = ({children, toggleShow, show}: BackdropProp) => {
+const Backdrop = ({ children, toggleShow, show }: BackdropProp) => {
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation()
+  }
   return (
-    show && <div className="backdrop-container" onClick={toggleShow}>{children}</div> 
+    show && (
+      <div className="backdrop-container" onClick={toggleShow}>
+        <div className="backdrop-content" onClick={handleClick}>{children}</div>
+      </div>
+    )
   )
 }
 
